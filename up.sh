4b89f02c995a9148fc1e3f3ea74fb09fa24a5396
Ethan Apodaca <papodaca@gmail.com>
2021-08-22 00:12:06 +0000

    Add an option to use flannel rather than calico

--- up.sh
+++ up.sh
@@ -4,11 +4,16 @@ k8s_version=1.21.1
 flanel="https://raw.githubusercontent.com/coreos/flannel/master/Documentation/kube-flannel.yml"
 calico="https://docs.projectcalico.org/manifests/calico.yaml"
 
+cni_plugin=$calico
+if [[ -z "${FLANEL}" ]]; then
+  cni_plugin=$flanel
+fi
+
 vagrant up
 vagrant ssh-config > .vagrant/ssh_config
 
 ssh -F .vagrant/ssh_config master "sudo kubeadm init --pod-network-cidr 192.168.100.0/24 --kubernetes-version ${k8s_version}"
-ssh -F .vagrant/ssh_config master "sudo KUBECONFIG=/etc/kubernetes/admin.conf kubectl apply -f https://docs.projectcalico.org/manifests/calico.yaml"
+ssh -F .vagrant/ssh_config master "sudo KUBECONFIG=/etc/kubernetes/admin.conf kubectl apply -f ${cni_plugin}"
 
 command=$(ssh -F .vagrant/ssh_config master "sudo kubeadm token create --print-join-command")
 ssh -F .vagrant/ssh_config worker_1 "sudo ${command}" &
